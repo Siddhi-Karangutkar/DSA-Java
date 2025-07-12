4dd80aff27296419f510b545ca71ddff6529d375
Siddhi-Karangutkar <karangutkarsiddhi19@gmail.com>
2025-07-12 14:35:17 +0530

    Create 242_ValidAnagram.java   Difficulty: Easy
    link : https://leetcode.com/problems/valid-anagram/
    
    
    description : Given two strings s and t, return true if t is an anagram of s, and false otherwise.
    
    
    Constraints:
    
    1 <= s.length, t.length <= 5 * 10^4
    s and t consist of lowercase English letters.
    
    
    Topics : 
    
    Hash Table
    String
    Sorting

---
+++ Strings/242_ValidAnagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char ch1[] = s.toCharArray();
+        char ch2[] = t.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        s = new String(ch1);
+        t = new String(ch2);
+
+        if(s.equals(t)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
