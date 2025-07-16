67989fb55c51f38bd4fbc742dd87ae5b73739d72
Siddhi-Karangutkar <karangutkarsiddhi19@gmail.com>
2025-07-16 22:44:15 +0530

    Create 1796_SecondLargestDigitInAString.java   Difficulty: Easy
    link : https://leetcode.com/problems/second-largest-digit-in-a-string/description/
    
    
    
    description : Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
    
    An alphanumeric string is a string consisting of lowercase English letters and digits.
    
    
    Constraints:
    1 <= s.length <= 500
    s consists of only lowercase English letters and digits.
    
    
    Topics : 
    Hash Table
    String

---
+++ 1796_SecondLargestDigitInAString.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int secondHighest(String s) {
+        char ch[] = s.toCharArray();
+        int n = ch.length;
+        int max=-1;
+        int secondmax=-1;
+        boolean flag = false;
+        for(int i=0;i<n;i++){
+            if(Character.isDigit(ch[i])){
+                if(flag==false){
+                    max = Integer.parseInt(""+ch[i]);
+                    flag=true;
+                }else{
+                    if(Integer.parseInt(""+ch[i])>max){
+                        secondmax=max;
+                        max=Integer.parseInt(""+ch[i]);
+                    }else if(Integer.parseInt(""+ch[i])>secondmax && Integer.parseInt(""+ch[i])<max){
+                        secondmax=Integer.parseInt(""+ch[i]);
+                    }
+                }
+            }
+        } 
+        
+        if(secondmax==-1){
+            return -1;
+        }else{
+        return secondmax;
+
+        }
+
+    }
+}
