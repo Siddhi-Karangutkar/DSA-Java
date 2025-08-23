94226f41943df836f15251944deeaf01c6c5e731
Siddhi-Karangutkar <karangutkarsiddhi19@gmail.com>
2025-08-23 20:26:37 +0530

    Create 2011_FinalValueOfVariableAfterPerformingOperations.java   Difficulty : Easy
    link : https://leetcode.com/problems/final-value-of-variable-after-performing-operations/description/
    
    description : There is a programming language with only four operations and one variable X:
    ++X and X++ increments the value of the variable X by 1.
    --X and X-- decrements the value of the variable X by 1.
    Initially, the value of X is 0.
    Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
    
    Constraints : 
    1 <= operations.length <= 100
    operations[i] will be either "++X", "X++", "--X", or "X--".
    
    Topics : 
    Array
    String
    Simulation
    Weekly Contest 259

---
+++ Strings/2011_FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int n = operations.length;
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+            if(operations[i].equals("--X") || operations[i].equals("X--")){
+                ans = ans -1;
+            }else{
+                ans = ans + 1;
+            }
+        }
+        return ans;
+    }
+}
