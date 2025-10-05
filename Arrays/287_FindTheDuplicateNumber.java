592a20987cf66f7257ab34609ff28e5c63e8f558
Siddhi-Karangutkar <karangutkarsiddhi19@gmail.com>
2025-10-05 18:21:04 +0530

    Create 287_FindTheDuplicateNumber.java  Difficulty : Medium
    link : https://leetcode.com/problems/find-the-duplicate-number/description/
    
    description : Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
    There is only one repeated number in nums, return this repeated number.
    You must solve the problem without modifying the array nums and using only constant extra space.
    
    Constraints : 
    1 <= n <= 10^5
    nums.length == n + 1
    1 <= nums[i] <= n
    All the integers in nums appear only once except for precisely one integer which appears two or more times.
    
    Topics : 
    Array
    Two Pointers
    Binary Search
    Bit Manipulation

---
+++ Arrays/287_FindTheDuplicateNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        
+        for(int i = 0;i<n;i++){
+            int index = Math.abs(nums[i]);
+            if(nums[index]>0){
+                nums[index] = -nums[index];
+            }  else{
+                return Math.abs(nums[i]);
+            }
+        }
+
+        return -1;
+    }
+}
