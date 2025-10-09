f93796d2ad9d4ba0e5a5aa54fef4746d61c3fbb0
Siddhi-Karangutkar <karangutkarsiddhi19@gmail.com>
2025-10-09 22:01:01 +0530

    Create 852_PeakIndexInAMountainArray.java     Difficulty : Medium
    link : https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
    
    Description : You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
    Return the index of the peak element.
    Your task is to solve it in O(log(n)) time complexity.
    
    Constraints : 
    3 <= arr.length <= 10^5
    0 <= arr[i] <= 10^6
    arr is guaranteed to be a mountain array.
    
    Topics : 
    Array
    Binary Search
    Weekly Contest 89

---
+++ Arrays/852_PeakIndexInAMountainArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+
+        while(left<right){
+            int mid = (left+right)/2;
+            if(arr[mid-1]<arr[mid] && arr[mid+1]<arr[mid]){
+                return mid;
+            }else if(arr[mid-1]>arr[mid]){
+                right = mid;
+            }else{
+                left = mid;
+            }
+
+        }
+        
+
+        return 0;
+    }
+}
