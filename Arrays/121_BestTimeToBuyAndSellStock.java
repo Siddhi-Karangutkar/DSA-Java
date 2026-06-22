fc84097f725f0c1f683d9d6fd9555a35eef6c856
Siddhi-Karangutkar <karangutkarsiddhi19@gmail.com>
2026-06-22 16:29:16 +0530

    Update 121_BestTimeToBuyAndSellStock.java      Difficulty : Easy
    link : https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/
    
    description : Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
    
     Constraints:
    1 <= arr.length <= 10^4
    0 <= arr[i] <= 10^5
    
    Topics : 
    Junior
    Array
    Biweekly Contest 15

--- Arrays/121_BestTimeToBuyAndSellStock.java
+++ Arrays/121_BestTimeToBuyAndSellStock.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
+        
+        int n = prices.length; 
         int max = 0;
         int min = prices[0];
         for(int i = 1;i<n;i++){
