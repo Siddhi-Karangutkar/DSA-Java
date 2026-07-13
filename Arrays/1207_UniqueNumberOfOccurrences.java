e9875b8e03acac41590fa88909626e617cb7fe90
Siddhi-Karangutkar <karangutkarsiddhi19@gmail.com>
2026-07-13 20:48:43 +0530

    Create 1207_UniqueNumberOfOccurrences.java    Difficulty : Easy
    link : https://leetcode.com/problems/unique-number-of-occurrences/description/
    
    description : Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
    
     Constraints : 
    1 <= arr.length <= 1000
    -1000 <= arr[i] <= 1000
    
    Topics : 
    Mid Level
    Array
    Hash Table
    Weekly Contest 156

---
+++ Arrays/1207_UniqueNumberOfOccurrences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0;i<n;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],1);
+            }else{
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+        }
+
+        for(int i : map.values()){
+            if(!set.contains(i)){
+                set.add(i);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
